7556669ed35a33f4c39111e38facc658de930a7f
Sergey <xxsmotur@gmail.com>
2019-09-27 07:22:06 +0500

    fixing of arguments

--- generator/generator.py
+++ generator/generator.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-def generate_rows(conn, n):
+def generate_rows(conn, table_name, n):
     with conn.cursor() as cursor:
         conn.autocommit = True
         values = [
@@ -8,10 +8,10 @@ def generate_rows(conn, n):
             ('TSE', 'Astana', 'Kazakhstan'),
             ('PDX', 'Portland', 'USA'),
         ]
-        insert = sql.SQL('INSERT INTO city (code, name, country_name) VALUES {}').format(
-            sql.SQL(',').join(map(sql.Literal, values))
+        insert = sql.SQL('INSERT INTO {} (code, name, country_name) VALUES {}').format(
+            table_name, sql.SQL(',').join(map(sql.Literal, values))
         )
         cursor.execute(insert)
 
 conn = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='localhost')
-cursor = conn.cursor()
+generate_rows(conn, 10)
