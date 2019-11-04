588012915b1f6690cf63cf7cd6ae35303bf9ea59
Sergey <xxsmotur@gmail.com>
2019-11-04 15:10:53 +0500

    container start

--- generator/generator.py
+++ generator/generator.py
@@ -1,5 +1,23 @@
 import psycopg2
 
+
+def create_table():
+    commands = (
+        """
+        CREATE TABLE IF NOT EXIST customers(
+            id INTEGER PRIMARY KEY,
+            first_name text,
+            last_name text,
+            created_at timestampz NOT NULL,
+            info jsonb,
+            priority int4,
+        )
+        """
+
+        with conn.cursor() as cursor:
+            for data in commands:
+                conn.execute(data)
+    )
 def generate_rows(conn, table_name, n):
     with conn.cursor() as cursor:
         conn.autocommit = True
